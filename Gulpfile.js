09de36f229142a28c030606958fe650f2bd9fc0e
Gustavo Ramirez Rugeiro <gustavoramirezrugerio@gmail.com>
2015-06-22 23:42:49 -0500

    Minificando archivos css, js, html y comprimiendo imagenes. tambien semodifico el SCRIP para realizar estas operaciones

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,99 +1,97 @@
 // incluir plug-ins de gulp  http://gulpjs.com/plugins/
-var gulp      = require('gulp'),
-  jshint      = require('gulp-jshint'),
-  uglify      = require('gulp-uglify'),
-  changed     = require('gulp-changed')
-  imagemin    = require('gulp-imagemin'),
-  stripDebug  = require('gulp-strip-debug'),
-  minifyCSS   = require('gulp-minify-css'),
-  minifyHTML  = require('gulp-minify-html'),
-  concat      = require('gulp-concat'),
-  rename      = require('gulp-rename'),
-  browserify  = require('gulp-browserify');
+var gulp        = require('gulp'),
+jshint          = require('gulp-jshint'),
+uglify          = require('gulp-uglify'),
+changed         = require('gulp-changed')
+imagemin        = require('gulp-imagemin'),
+stripDebug      = require('gulp-strip-debug'),
+minifyCSS       = require('gulp-minify-css'),
+minifyHTML      = require('gulp-minify-html'),
+rename          = require('gulp-rename'),
+browserify      = require('gulp-browserify');
+var minifyInline = require('gulp-minify-inline');
 
 //::::::::::::::::::::: Creacion de tareas "development"
+var scr_origen  = "sitio";
+var scr_destino = "production";
+
 gulp.task('jshint', function() {
-  gulp.src('./development/js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+gulp.src(scr_origen+'/assets/js/*.js')
+.pipe(jshint())
+.pipe(jshint.reporter('default'));
 });
 
 //::::::::::::::::::::: Creacion de tareas "production"
 gulp.task('minificarJS', function () {
-  gulp.src('../sitio_produccion/assets/js/**/*.js')
-    .pipe(browserify())
-    .pipe(uglify({ compress: true }))
-    .pipe(stripDebug())
-    .pipe(gulp.dest('.././production/js'));
+gulp.src(scr_origen+'/assets/js/**/*.js')
+.pipe(browserify())
+.pipe(uglify({ compress: true }))
+.pipe(stripDebug())
+.pipe(gulp.dest(scr_destino+'/assets/js'));
 });
 
 gulp.task('minificarVendorJS', function () {
-  gulp.src('./development/vendor/**/*.js')
-    .pipe(browserify())
-    .pipe(uglify({ compress: true }))
-    .pipe(stripDebug())
-    .pipe(gulp.dest('./production/vendor'));
-});
-
-gulp.task('scripts', function() {
-    return gulp.src('./development/js/**/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./production/js'));
+gulp.src(scr_origen+'/assets/vendors/**/*.js')
+.pipe(browserify())
+.pipe(uglify({ compress: true }))
+.pipe(stripDebug())
+.pipe(gulp.dest(scr_destino+'/assets/vendors'));
 });
 
 gulp.task('css', function () {
-  gulp.src('./development/css/**/*.css')
-    .pipe(minifyCSS({ keepSpecialComments: '*', keepBreaks: '*'}))
-    .pipe(gulp.dest('./production/css'));
+gulp.src(scr_origen+'/assets/css/**/*.css')
+.pipe(minifyCSS({ keepSpecialComments: '*', keepBreaks: '*'}))
+.pipe(gulp.dest(scr_destino+'/assets/css'));
 });
 
 gulp.task('minificarVendorCSS', function () {
-  gulp.src('./development/vendor/**/*.css')
-    .pipe(minifyCSS({ keepSpecialComments: '*', keepBreaks: '*'}))
-    .pipe(gulp.dest('./production/vendor/'));
+gulp.src(scr_origen+'/assets/vendors/**/*.css')
+.pipe(minifyCSS({ keepSpecialComments: '*', keepBreaks: '*'}))
+.pipe(gulp.dest(scr_destino+'/assets/vendors/'));
 });
 
 gulp.task('images', function () {
-  var imgSrc = './development/imagenes/**/*',
-      imgDst = './production/imagenes';
-
-  gulp.src('./development/imagenes/**/*')
-    .pipe(changed(imgDst))
-    .pipe(imagemin())
-    .pipe(gulp.dest(imgDst));
+var imgSrc      = scr_origen+'/assets/imagenes/**/*',
+imgDst          = scr_destino+'/assets/imagenes';
+gulp.src(scr_origen+'/assets/imagenes/**/*')
+.pipe(changed(imgDst))
+.pipe(imagemin())
+.pipe(gulp.dest(imgDst));
 });
 
 gulp.task('minificarVendorIMAGENES', function () {
-  var imgSrc = './development/vendor/**/*',
-      imgDst = './production/vendor';
+var imgSrc      = scr_origen+'/assets/vendors/**/*',
+imgDst          = scr_destino+'/assets/vendors/';
 
-  gulp.src('./development/vendor/**/*')
-    .pipe(changed(imgDst))
-    .pipe(imagemin())
-    .pipe(gulp.dest(imgDst));
+gulp.src(scr_origen+'/assets/vendors/**/*')
+.pipe(changed(imgDst))
+.pipe(imagemin())
+.pipe(gulp.dest(imgDst));
 });
 
 
-gulp.task('html', function () {
-  var htmlSrc = './development/*.html',
-      htmlDst = './production';
+gulp.task('minify-html', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
 
-  gulp.src(htmlSrc)
-  .pipe(minifyHTML())
-  .pipe(gulp.dest(htmlDst));
+  return gulp.src(scr_origen+'/*.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest(scr_destino+'/'));
 });
+
 
 gulp.task('fonts', function () { //solamente se copia
-  gulp.src('./development/fonts/**')
-    .pipe(gulp.dest('./production/fonts'));
+gulp.src( scr_origen+'/assets/fonts/*')
+.pipe(gulp.dest( scr_destino+'/assets/fonts/'));
 });
 
 
 //Agrupacion de tareas
-//gulp.task('default', [ 'minificarJS', 'minificarVendorJS', 'minificarVendorCSS', 'minificarVendorIMAGENES', 'css', 'images', 'html', 'fonts' ]);
-gulp.task('default', [ 'minificarJS',  ]);
+gulp.task('default', [ 'minificarJS', 'minificarVendorCSS', 'minificarVendorIMAGENES', 'css', 'images', 'minify-html', 'fonts' ]);
+//gulp.task('default', [ 'minificarVendorJS' ]);
+//
+//gulp.task('default', [ 'minify-html'  ]);
 //gulp.task('default', [ 'jshint' ]);
 
